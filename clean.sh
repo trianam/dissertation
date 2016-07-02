0029857452b13c1ad26c1d608cac1fb5373f0fa4
Stefano Martina <stefano.martina@gmail.com>
2016-07-02 18:32:55 +0200

    Added slides

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-rm -fR auto *.aux *.log *.toc *.nav *.out *.snm *.vrb img/*-converted-to.* *.bbl *.blg *.brf *.idx *.ilg *.ind chapters/auto chapters/*.aux chapters/*.log chapters/*.toc chapters/*.nav chapters/*.out chapters/*.snm chapters/*.vrb chapters/*.bbl chapters/*.blg chapters/*.brf chapters/*.idx chapters/*.ilg chapters/*.ind 
+rm -fR auto *.aux *.log *.toc *.nav *.out *.snm *.vrb img/*-converted-to.* *.bbl *.blg *.brf *.idx *.ilg *.ind chapters/auto chapters/*.aux chapters/*.log chapters/*.toc chapters/*.nav chapters/*.out chapters/*.snm chapters/*.vrb chapters/*.bbl chapters/*.blg chapters/*.brf chapters/*.idx chapters/*.ilg chapters/*.ind slides/auto slides/*.aux slides/*.log slides/*.toc slides/*.nav slides/*.out slides/*.snm slides/*.vrb slides/img/*-converted-to.*
 
